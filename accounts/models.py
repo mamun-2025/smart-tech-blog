aeb46d8163c8bdc6681099c41ed889937c2f44e0
Mamun <beparimamun708@gmail.com>
2026-02-26 16:09:55 +0600

    Fixed: Integrated Cloudinary for Profile and Post images, and updated bio

--- accounts/models.py
+++ accounts/models.py
@@ -1,10 +1,11 @@
 from django.db import models 
 from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 class Profile(models.Model):
    user = models.OneToOneField(User, on_delete=models.CASCADE)
    bio = models.TextField(blank=True)
-   profile_pic = models.ImageField(upload_to='profile_pics', default='default.jpg')
+   profile_pic = CloudinaryField('image', folder='profile_pics')
    website_url = models.URLField(max_length=200, blank=True)
 
    def __str__(self):
