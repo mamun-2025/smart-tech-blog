a61177164053190877686636098f73d2bb0a2580
Mamun <beparimamun708@gmail.com>
2026-02-26 20:53:05 +0600

    fixed settings.py

--- MainSettings/settings.py
+++ MainSettings/settings.py
@@ -23,7 +23,7 @@ DATABASES = {
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['smart-tech-blog.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -90,17 +90,6 @@ WSGI_APPLICATION = 'MainSettings.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
-    }
-}
 
 
 # Password validation
