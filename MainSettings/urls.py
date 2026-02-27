f2d117c5c29e23c42877808e81003a84473453bc
Mamun <beparimamun708@gmail.com>
2026-02-27 11:05:56 +0600

    Add superuser creation

--- MainSettings/urls.py
+++ MainSettings/urls.py
@@ -3,13 +3,24 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth.models import User
+from django.http import HttpResponse
+
+def create_superuser(request):
+    if not User.objects.filter(username='mamun_bepari').exists():
+        User.objects.create_superuser('mamun_bepari', 'beparimamun708@gmail.com', 'blog123@')
+        return HttpResponse("Superuser created successfully.")
+    else: 
+        return HttpResponse("Superuser already exists.")
 
 urlpatterns = [
     path('admin_security/', admin.site.urls),
     path('', include('blog.urls', namespace='blog')),
     path('accounts/', include('accounts.urls', )),
     path('accounts/', include('django.contrib.auth.urls', )),
+    path('make-me-admin/', create_superuser),
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
