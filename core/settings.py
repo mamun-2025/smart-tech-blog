9f65a4f19b5504ea215a77acf63b743a9adf243d
Mamun <beparimamun708@gmail.com>
2026-03-12 21:33:59 +0600

    Added Authentication settings

--- core/settings.py
+++ core/settings.py
@@ -116,3 +116,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
+
+# Authentication settings
+LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = 'post_list'
+LOGOUT_REDIRECT_URL = 'post_list'
